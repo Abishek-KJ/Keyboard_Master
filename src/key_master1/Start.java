d35e454d51925b1fb653be42a061a5b4ca645f6f
Lenovo <Lenovo@DESKTOP-EIENH60>
2023-09-01 11:42:47 +0530

    Expertise Level Created.

---
+++ src/key_master1/Start.java
@@ -0,0 +1,87 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package key_master1;
+
+/**
+ *
+ * @author Lenovo
+ */
+
+import javax.swing.JFrame; 
+import javax.swing.JLabel; 
+import javax.swing.ImageIcon; 
+import java.awt.Image; 
+import java.awt.Toolkit; 
+
+public class Start {
+    
+    public JFrame frameStart; 
+    
+    public JLabel introduction; 
+    
+    public ImageIcon loading; 
+    
+    public Image iconStart; 
+    
+    Start(){
+        
+        //JFrame Configuration Starts Here...................
+        
+        
+       frameStart = new JFrame(); 
+       
+       iconStart = new Image(); 
+       
+       iconStart = Toolkit.getImage(""); 
+       
+       
+       iconStart = ToolKIt.getDefaultToolkit.getImage("C:\\Users\\Lenovo\\OneDrive\\Documents\\NetBeansProjects\\Key_Master1\\src\\key_master1/Icon.png"); 
+       
+       iconStart = ToolKit.getDefaultToolKit.getImage("C:\\Users\\Lenovo\\OneDrive\\Documents\\NetBeansProjects\\Key_Master1\\src\\key_master1/Icon.png"); 
+        
+       iconStart = Toolkit.getDefaultToolkit().getImage("C:\\Users\\Lenovo\\OneDrive\\Documents\\NetBeansProjects\\Key_Master1\\src\\key_master1/Icon.png"); 
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       iconStart = Toolkit.getDefaultToolkit().getImage("C:\\Users\\Lenovo\\OneDrive\\Documents\\NetBeansProjects\\Key_Master1\\src\\key_master1/Icon.png");
+       
+       
+       
+       
+       
+       
+        
+        
+        
+        
+        
+        
+    }
+    
+    
+    
+    
+}
