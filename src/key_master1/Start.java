f448e181ede2723cf77c2c81ceddaa7ded550704
Lenovo <Lenovo@DESKTOP-EIENH60>
2023-09-11 23:39:17 +0530

    Expertise Level Created.

--- src/key_master1/Start.java
+++ src/key_master1/Start.java
@@ -14,6 +14,20 @@ import javax.swing.JLabel;
 import javax.swing.ImageIcon; 
 import java.awt.Image; 
 import java.awt.Toolkit; 
+import java.awt.Color; 
+import javax.swing.Timer; 
+import java.awt.event.ActionListener; 
+import java.awt.event.ActionEvent; 
+import javax.swing.UIManager; 
+import java.lang.reflect.Method; 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.UnsupportedLookAndFeelException; 
+import java.lang.IllegalAccessException; 
+import java.lang.InstantiationException; 
+
+import defaultpackage.About; 
+
 
 public class Start {
     
@@ -25,6 +39,8 @@ public class Start {
     
     public Image iconStart; 
     
+   
+    
     Start(){
         
         //JFrame Configuration Starts Here...................
@@ -32,7 +48,7 @@ public class Start {
         
        frameStart = new JFrame(); 
        
-       iconStart = new Image(); 
+      /* iconStart = new Image(); 
        
        iconStart = Toolkit.getImage(""); 
        
@@ -41,8 +57,29 @@ public class Start {
        
        iconStart = ToolKit.getDefaultToolKit.getImage("C:\\Users\\Lenovo\\OneDrive\\Documents\\NetBeansProjects\\Key_Master1\\src\\key_master1/Icon.png"); 
         
+       iconStart = Toolkit.getDefaultToolkit().getImage("C:\\Users\\Lenovo\\OneDrive\\Documents\\NetBeansProjects\\Key_Master1\\src\\key_master1/Icon.png"); */ 
+       
        iconStart = Toolkit.getDefaultToolkit().getImage("C:\\Users\\Lenovo\\OneDrive\\Documents\\NetBeansProjects\\Key_Master1\\src\\key_master1/Icon.png"); 
        
+       loading = new ImageIcon("C:\\Users\\Lenovo\\OneDrive\\Documents\\NetBeansProjects\\Key_Master1\\src\\key_master1/Introduction.gif"); 
+       
+       
+       frameStart = new JFrame(); 
+       frameStart.setIconImage(iconStart); 
+       frameStart.setResizable(false); 
+       //frameStart.setBounds(r);
+       //frameStart.setSize(d); 
+       frameStart.setSize(551,314); 
+       frameStart.setLocationRelativeTo(null); 
+       frameStart.getContentPane().setBackground(Color.GRAY); 
+       frameStart.setLayout(null); 
+       frameStart.setUndecorated(true); 
+       frameStart.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE); 
+       
+       
+       introduction = new JLabel();
+       introduction.setBounds(0,0,551,314); 
+       introduction.setIcon(loading); 
        
        
        
@@ -56,25 +93,87 @@ public class Start {
        
        
        
+       frameStart.add(introduction); 
+       
+       frameStart.setVisible(true); 
        
        
+       int splashScreen = 6000; //Milliseconds 
+       
+       //byte splashScreen1 = (byte) 6000; //Milliseconds 
+       
+       //Timer timer = new Timer(splashScreen1, new ActionListener(){ 
+       
+       Timer timer = new Timer(splashScreen, new ActionListener(){     
+       
+       //Timer timer = new Timer(10000, new ActionListener(){ 
+           @Override
+           public void actionPerformed(ActionEvent e){
+               frameStart.dispose(); 
+               
+               try{
+                  UIManager.setLookAndFeel("com.jtattoo.plaf.acryl.AcrylLookAndFeel");
+               } 
+               catch(Exception exception){
+                   System.out.println(exception); 
+               }
+              /* catch(ClassNotFoundException exception1 || InstantiationException exception2 || IllegalAccessException exception3 || UnsupportedLookAndFeelException exception4){ 
+                   System.out.println(exception); 
+               } 
+               /*catch (InstantiationException ex) {
+                   Logger.getLogger(Start.class.getName()).log(Level.SEVERE, null, ex);
+               } catch (IllegalAccessException ex) {
+                   Logger.getLogger(Start.class.getName()).log(Level.SEVERE, null, ex);
+               } catch (UnsupportedLookAndFeelException ex) {
+                   Logger.getLogger(Start.class.getName()).log(Level.SEVERE, null, ex);
+               } */ 
+              // Levels1 levels1 = new Levels1(); 
+              // levels1.openPlayground1(); 
+               for(int i = 2; i <= 60; i++){
+                  // levels1.openPlayground1(); 
+               } 
+               
+              try{
+              Levels1 lev1 = new Levels1(); 
+              
+              AboutTheSoftware about = new About(); 
+              
+              Method[] methods = lev1.getClass().getDeclaredMethods(); 
+              
+              //Method[] methods = about.getClass().getDeclaredMethods(); 
+              
+              for(Method method : methods){
+                   if(method.getName().startsWith("openPlayground")){
+                      method.invoke(lev1, (Object[]) null); 
+                   } 
+                   
+                   if(method.getName().startsWith("AboutTheSoftware")){ 
+                       method.invoke(about, (Object[]) null); 
+                   } 
+              }
+              }
+              catch(Exception exception){
+                  System.out.println(exception); 
+              }
+              
+              //AboutTheSoftware about = new About(); 
+              //about.AboutTheSoftware(); 
+              
+               
+           }
+       });
+       
+       timer.setRepeats(false); 
+       timer.start(); 
        
        
-       
-       
-       
-       
-       
-       
-       iconStart = Toolkit.getDefaultToolkit().getImage("C:\\Users\\Lenovo\\OneDrive\\Documents\\NetBeansProjects\\Key_Master1\\src\\key_master1/Icon.png");
-       
-       
-       
-       
-       
-       
-        
-        
+     // Levels1 levels1 = new Levels();  
+      
+    
+     
+     // Levels1 levels1 = new Levels1(); 
+      
+      
         
         
         
@@ -82,6 +181,43 @@ public class Start {
     }
     
     
+    public static void main(String[] args) throws Exception{ 
+      //  try{
+        //UIManager.setLookAndFeel("com.jtattoo.plaf.acryl.AcrylLookAndFeel"); 
+        Start start = new Start();  
+    //    }
+        //catch(ClassNotFoundException exception){
+   //         System.out.println(exception); 
+        //} 
+        //finally{
+        //    System.out.println(); 
+      //  } 
+        
+       
+       // Levels1 levels1 = new Levels1(); 
+    }
     
     
-}
+    
+}   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
